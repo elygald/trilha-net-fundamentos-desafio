e282617af3a91b324e3457ddc55485d397716aaf
Ely Galdino <elygald@gmail.com>
2024-09-21 11:16:34 -0300

    desafio concluido ...

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -14,9 +17,18 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine().ToUpper();
+            bool isValida = ValidarPlaca(placa);
+            if (!isValida)
+            {
+                Console.WriteLine($"A placa {placa} é válida? {isValida}");
+                Console.WriteLine("Placa inválida. Digite uma placa no formato ABC-1234");
+                AdicionarVeiculo();
+                return;
+            }
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
@@ -24,28 +36,26 @@ namespace DesafioFundamentos.Models
             Console.WriteLine("Digite a placa do veículo para remover:");
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
             string placa = "";
-
+            placa = Console.ReadLine();
+            
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
+                // Pedir para o usuário digitar a quantidade de horas e armazenar na variável horas      
                 int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
+                decimal valorTotal = 0;
+                horas = Convert.ToInt32(Console.ReadLine());
+                valorTotal = precoInicial + precoPorHora * horas;
+                // Remover o veículo da lista e exibir a mensagem de remoção
+                veiculos.Remove(placa.ToUpper());
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine($"Desculpe, esse veículo com placa {placa} não está estacionado aqui. Confira se digitou a placa corretamente");
             }
         }
 
@@ -55,13 +65,27 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+        }
+
+        public static bool ValidarPlaca(string placa)
+        {
+            // Define a expressão regular para três letras seguidas por um hífen
+            string padrao = @"^[A-Za-z]{3}-\d{4}$";
+            
+            // Cria uma instância de Regex com o padrão definido
+            Regex regex = new Regex(padrao);
+            
+            // Verifica se a placa corresponde ao padrão
+            return regex.IsMatch(placa);
         }
     }
 }
